f3cb04799b975f0cb1d9eabd80ebafc1f5ebb690
Jungin1020 <97022661+Jungin1020@users.noreply.github.com>
2023-06-12 17:48:36 +0900

    021: 파일 나누기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inflearn_flutter_basic/my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,75 +19,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-// stful Live Template으로 코드 생성
-// 화면에 그리는 것 중 항상 변하는 것이 있다면 사용하는 위젯
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // 앱을 만들 때 기본이 되는 뼈대를 제공하는 위젯
-      appBar: AppBar(
-        title: Text('홈'),
-      ),
-      body: Center(
-        // option + Enter로 Column으로 감싸기
-        child: Column(
-          // Column 내부에 있는 요소를 정렬
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '숫자',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 30,
-              ),
-            ),
-            Text(
-              '$count',
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 60,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('Elevated Button');
-              },
-              child: Text('Elevated Button'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('Text Button'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('Outlined Button'),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // 화면 갱신
-          setState(() {
-            count++;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
