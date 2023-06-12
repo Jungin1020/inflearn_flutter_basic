70d0dd24742c0c6383223110eeccfb42cd130d9e
Jungin1020 <97022661+Jungin1020@users.noreply.github.com>
2023-06-12 20:11:58 +0900

    27: 머터리얼 디자인이란?

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // ios는 Cupertino
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
         useMaterial3: true,
       ),
       home: const MainPage(),
