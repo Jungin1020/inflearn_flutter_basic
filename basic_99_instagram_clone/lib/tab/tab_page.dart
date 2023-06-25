ac69817fb0c000bfc50036347bb91b793caa73ad
Jungin1020 <97022661+Jungin1020@users.noreply.github.com>
2023-06-26 00:13:57 +0900

    FEAT: 뼈때 작성

--- basic_99_instagram_clone/lib/tab/tab_page.dart
+++ basic_99_instagram_clone/lib/tab/tab_page.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+class TabPage extends StatefulWidget {
+  const TabPage({Key? key}) : super(key: key);
+
+  @override
+  State<TabPage> createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search_outlined),
+            label: 'search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet_outlined),
+            label: 'account',
+          ),
+        ],
+      ),
+    );
+  }
+}
