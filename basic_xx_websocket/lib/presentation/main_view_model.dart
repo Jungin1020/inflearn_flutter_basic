7daadb77116f2eed595e29726a5db5a287c30e8f
Jungin1020 <97022661+Jungin1020@users.noreply.github.com>
2023-07-13 21:33:37 +0900

    [WebSocket 예시] MainViewModel 작성

---
+++ basic_xx_websocket/lib/presentation/main_view_model.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'package:basic_xx_websocket/domain/repository/price_repository.dart';
+import 'package:flutter/foundation.dart';
+import '../domain/model/price.dart';
+
+class MainViewModel with ChangeNotifier {
+  final PriceRepository _priceRepository;
+
+  MainViewModel(this._priceRepository);
+
+  StreamSubscription<Price>? _subscription;
+
+  final List<Price> _prices = [];
+
+  List<Price> get prices => List.unmodifiable(_prices);
+
+  void fetch() {
+    _subscription = _priceRepository.getPriceStream().listen((price) {
+      _prices.add(price);
+      notifyListeners();
+    });
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+}
