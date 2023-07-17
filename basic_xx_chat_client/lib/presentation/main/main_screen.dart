e535155138e8fd08a91760c1bf58960ca402e973
Jungin1020 <97022661+Jungin1020@users.noreply.github.com>
2023-07-17 18:19:12 +0900

    [Chat Client] MainScreen 꾸미기, TextField 비워지는 코드 작성

--- basic_xx_chat_client/lib/presentation/main/main_screen.dart
+++ basic_xx_chat_client/lib/presentation/main/main_screen.dart
@@ -30,40 +30,98 @@ class _MainScreenState extends State<MainScreen> {
       body: Column(
         children: [
           Expanded(
-            child: ListView(
-              children: viewModel.messages
-                  .map(
-                    (e) => ListTile(
-                      title: Text(e.message),
-                    ),
-                  )
-                  .toList(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ListView(
+                reverse: true,
+                children: viewModel.messages
+                    .map(
+                      (e) => SizedBox(
+                        height: 56,
+                        child: ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Column(
+                                children: [
+                                  Center(
+                                    child: AnimatedContainer(
+                                      height: 45,
+                                      decoration: BoxDecoration(
+                                        color: Colors.grey.shade100,
+                                        borderRadius: const BorderRadius.only(
+                                          topLeft: Radius.circular(20),
+                                          topRight: Radius.circular(20),
+                                          bottomLeft: Radius.circular(20),
+                                        ),
+                                      ),
+                                      padding: const EdgeInsets.all(10),
+                                      duration: const Duration(seconds: 1),
+                                      curve: Curves.easeInOut,
+                                      child: Text(
+                                        e.message,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(width: 10),
+                              CircleAvatar(
+                                radius: 18,
+                                backgroundColor: Colors.grey,
+                                child: CircleAvatar(
+                                  radius: 16,
+                                  backgroundColor: Colors.grey.shade200,
+                                  // foregroundColor: Colors.white,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const SizedBox(
+                                        height: 0.5,
+                                      ),
+                                      Text('정인',
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.grey.shade600,
+                                          )),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList()
+                    .reversed
+                    .toList(),
+              ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32.0),
-            child: Row(
-              children: [
-                Container(
-                    width: 100,
-                    child: TextField(controller: nameTextController)),
-                SizedBox(width: 20),
-                Expanded(
-                  child: TextField(
-                    controller: messageTextController,
-                    decoration: InputDecoration(
-                        suffixIcon: IconButton(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: SizedBox(
+              height: 56,
+              child: TextField(
+                controller: messageTextController,
+                decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                    hintText: '메세지를 입력하세요',
+                    border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(40))),
+                    suffixIcon: IconButton(
                       icon: const Icon(Icons.send_outlined),
                       onPressed: () {
                         viewModel.sendMessage(
                           nameTextController.text,
                           messageTextController.text,
                         );
+                        messageTextController.clear();
                       },
                     )),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
           const SizedBox(height: 40),
