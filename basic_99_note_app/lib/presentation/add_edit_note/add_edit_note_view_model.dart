50cb65370d4d66c22b785ffeaecc6937436ab5f3
Jungin1020 <97022661+Jungin1020@users.noreply.github.com>
2023-07-18 20:40:36 +0900

    [노트앱_클린] add_edit_note event, viewModel 작성

---
+++ basic_99_note_app/lib/presentation/add_edit_note/add_edit_note_view_model.dart
@@ -0,0 +1,45 @@
+import 'package:basic_99_note_app/domain/repository/note_repository.dart';
+import 'package:basic_99_note_app/presentation/add_edit_note/add_edit_note_event.dart';
+import 'package:flutter/material.dart';
+
+import '../../domain/model/note.dart';
+
+class AddEditNoteViewModel with ChangeNotifier {
+  final NoteRepository repository;
+
+  int _color = Colors.orange.value;
+
+  int get color => _color;
+
+  AddEditNoteViewModel(this.repository);
+
+  void onEvent(AddEditNoteEvent event) {
+    event.when(changeColor: _changeColor, saveNote: _saveNote);
+  }
+
+  Future<void> _changeColor(int color) async {
+    _color = color;
+    notifyListeners();
+  }
+
+  Future<void> _saveNote(int? id, String title, String content) async {
+    if (id == null) {
+      repository.insertNote(
+        Note(
+          title: title,
+          content: content,
+          timestamp: DateTime.now().microsecondsSinceEpoch,
+          color: _color,
+        ),
+      );
+    } else {
+      await repository.updateNote(Note(
+        id: id,
+        title: title,
+        content: content,
+        timestamp: DateTime.now().microsecondsSinceEpoch,
+        color: _color,
+      ));
+    }
+  }
+}
