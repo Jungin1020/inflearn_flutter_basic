f1e3c46e345480fe9abec2fa5c3bd1e4cd26afd6
Jungin1020 <97022661+Jungin1020@users.noreply.github.com>
2023-07-20 19:28:30 +0900

    [Injectable] annotation 작성

--- basic_xx_injectable_sample/lib/di/di_setup.dart
+++ basic_xx_injectable_sample/lib/di/di_setup.dart
@@ -1,29 +1,3 @@
-// import 'package:basic_xx_get_it_sample/data/counter_repository.dart';
-// import 'package:basic_xx_get_it_sample/data/counter_repository_impl.dart';
-// import 'package:get_it/get_it.dart';
-//
-// import '../data/counter.dart';
-//
-// final getIt = GetIt.instance;
-//
-// Future<void> setup() async {
-//   // 싱글턴
-//   // getIt.registerSingleton<Counter>(Counter());
-//
-//   // 매번 생성
-//   // getIt.registerFactory<Counter>(() => Counter());
-//
-//   // 1
-//   getIt.registerSingleton<Counter>(Counter());
-//
-//   getIt.registerSingleton<CounterRepository>(
-//       CounterRepositoryImpl(getIt<Counter>()));
-//   // 2
-//   getIt.registerSingleton<CounterRepository>(CounterRepositoryImpl(Counter()));
-//
-//   // 1, 2 코드는 결과적으로는 같은 듯?
-// }
-
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
