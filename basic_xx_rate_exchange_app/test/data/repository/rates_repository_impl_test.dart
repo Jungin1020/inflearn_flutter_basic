31328d49f5964dd3b1d9e10193ff70ccc50d5165
Jungin1020 <97022661+Jungin1020@users.noreply.github.com>
2023-08-02 00:33:03 +0900

    [환율앱] chore

--- basic_xx_rate_exchange_app/test/data/repository/rates_repository_impl_test.dart
+++ basic_xx_rate_exchange_app/test/data/repository/rates_repository_impl_test.dart
@@ -5,6 +5,7 @@ void main() {
   test('객체로 잘 변형되는지 확인', () async {
     final repository = RatesRepositoryImpl();
     final ratesResult = await repository.getRateResult();
+    print(ratesResult);
     expect(ratesResult.rates['USD'], 1);
     expect(ratesResult.rates['KRW'], 1275.514182);
   });
